6743d9afa9940c70725946176bcb7aa7a823713d
rupesh-papneja <rupesh.papneja@gmail.com>
2017-03-30 18:41:21 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express = require('express');
 import { ServerApp } from './server';
 import process = require('process');
-let port = process.env.PORT; //or from a configuration file
+let port = process.env.PORT || 8000; //or from a configuration file
 let api = new ServerApp(express(), port);
 api.run();
 console.info(`listening on ${port}`);
